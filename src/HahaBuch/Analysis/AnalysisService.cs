98338b8dad5864a6b526b7fc3b2da6f3216f8c86
Simon Ge <noreply@simongensler.de>
2025-11-05 21:37:24 +0100

    Use localized "other" category for uncategorized transactions

--- src/HahaBuch/Analysis/AnalysisService.cs
+++ src/HahaBuch/Analysis/AnalysisService.cs
@@ -1,10 +1,11 @@
 using HahaBuch.Data;
 using HahaBuch.SharedContracts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 
 namespace HahaBuch.Analysis;
 
-public class AnalysisService(IDbContextFactory<ApplicationDbContext> dbContextFactory, VaultAccessor vaultAccessor) : IAnalysisService
+public class AnalysisService(IDbContextFactory<ApplicationDbContext> dbContextFactory, VaultAccessor vaultAccessor, IStringLocalizer<SharedResources> Loc) : IAnalysisService
 {
     public async Task<List<CategoryYearTotalDto>> GetCategoryYearTotals(int year)
     {
@@ -21,7 +22,7 @@ public class AnalysisService(IDbContextFactory<ApplicationDbContext> dbContextFa
             .Select(g => new CategoryYearTotalDto
             {
                 CategoryId = g.Key ?? Guid.Empty,
-                Category = g.First().Category != null ? g.First().Category!.Name : "(Uncategorized)",
+                Category = g.First().Category != null ? g.First().Category!.Name : Loc["Other"],
                 CategoryColor = g.First().Category != null ? g.First().Category!.Color : "000000",
                 Year = year,
                 ExpenseTotal = -g.Where(t => t.Amount < 0).Sum(t => t.Amount), // negate to make positive
